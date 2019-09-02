b43b0932ff23716bd342d134d752605c73fe981d
rishyahluwalia <rishy.ahluwalia26@gmail.com>
2019-09-03 02:19:00 +1000

    implement fetch posts function with backend rest api

--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -1,12 +1,15 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
-import { AngularMaterialModule } from '../shared/angular-material.module';
-import { NavComponent } from './components/nav/nav.component';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { RouterModule } from "@angular/router";
+import { AngularMaterialModule } from "../shared/angular-material.module";
+import { NavComponent } from "./components/nav/nav.component";
+import { PostService } from "./services/post.service";
+import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
+  imports: [CommonModule, AngularMaterialModule, RouterModule, HttpClientModule],
+  providers: [PostService],
   declarations: [NavComponent],
-  imports: [CommonModule, AngularMaterialModule, RouterModule],
   exports: [NavComponent]
 })
 export class CoreModule {}
--- client/src/app/features/discussion-board/discussion-board.module.ts
+++ client/src/app/features/discussion-board/discussion-board.module.ts
@@ -1,19 +1,13 @@
-import { NgModule } from '@angular/core';
-import { DiscussionBoardComponent } from './discussion-board.component';
-import { LeaderboardWidgetComponent } from './components/leaderboard-widget/leaderboard-widget.component';
-import { NewPostWidgetComponent } from './components/new-post-widget/new-post-widget.component';
-import { PostFeedComponent } from './components/post-feed/post-feed.component';
-import { PostFeedItemComponent } from './components/post-feed-item/post-feed-item.component';
-import { SharedModule } from 'src/app/shared/shared.module';
+import { NgModule } from "@angular/core";
+import { DiscussionBoardComponent } from "./discussion-board.component";
+import { LeaderboardWidgetComponent } from "./components/leaderboard-widget/leaderboard-widget.component";
+import { NewPostWidgetComponent } from "./components/new-post-widget/new-post-widget.component";
+import { PostFeedComponent } from "./components/post-feed/post-feed.component";
+import { PostFeedItemComponent } from "./components/post-feed-item/post-feed-item.component";
+import { SharedModule } from "src/app/shared/shared.module";
 
 @NgModule({
-  declarations: [
-    DiscussionBoardComponent,
-    LeaderboardWidgetComponent,
-    NewPostWidgetComponent,
-    PostFeedComponent,
-    PostFeedItemComponent
-  ],
+  declarations: [DiscussionBoardComponent, LeaderboardWidgetComponent, NewPostWidgetComponent, PostFeedComponent, PostFeedItemComponent],
   imports: [SharedModule]
 })
 export class DiscussionBoardModule {}
