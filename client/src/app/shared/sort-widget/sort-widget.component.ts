82a359d8791699bf896007000220b5d8fa6b0b03
Srishti Suri <srishti301@gmail.com>
2019-10-19 23:45:19 +1100

    code comments

--- client/src/app/shared/sort-widget/sort-widget.component.ts
+++ client/src/app/shared/sort-widget/sort-widget.component.ts
@@ -6,6 +6,8 @@ import { Component, OnInit, Input, EventEmitter, Output } from "@angular/core";
   styleUrls: ["./sort-widget.component.css"]
 })
 export class SortWidgetComponent implements OnInit {
+  //Visible on various pages throughout application, hence view is dynamic
+  //Button clicks are handled and sort occurs in the specific components
   @Input() sortTypes;
   @Input() sortType;
   @Output() handleSortBy = new EventEmitter();
